e95d932e0de32f1565ce5ed037ccd840c3a967a0
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-01-27 09:07:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+console.log(`Birthday`);
+let birthYear = prompt("When is your birthYear?");
+
+birthYear = parseInt(birthYear);
+
+let currentYear = new Date().getFullYear();
+
+let age = currentYear - birthYear;
+
+console.log(`You are ${age} years old.`);
